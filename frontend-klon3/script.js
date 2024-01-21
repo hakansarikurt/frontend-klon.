63149263f6eb08a2317ec2b88193e5dd21938d4e
hakansarikurt <148625342+hakansarikurt@users.noreply.github.com>
2024-01-21 03:25:09 +0300

    Add files via upload

---
+++ frontend-klon3/script.js
@@ -0,0 +1,219 @@
+
+
+// #################################################  CREW
+
+document.addEventListener("DOMContentLoaded", function () {
+    const crew1 = document.getElementById("crew1");
+    const crew2 = document.getElementById("crew2");
+    const crew3 = document.getElementById("crew3");
+
+    crew1.style.width = "100%";
+    crew1.style.display = "flex";
+
+
+
+    crew2.style.display = "none";
+    crew3.style.display = "none";
+});
+
+
+
+function bir() {
+    const crew1 = document.getElementById("crew1");
+    const crew2 = document.getElementById("crew2");
+    const crew3 = document.getElementById("crew3");
+    const btn1 = document.getElementById("btn1")
+
+    crew1.style.width = "100%";
+    crew1.style.display = "flex";
+
+    crew2.style.display = "none";
+    crew3.style.display = "none";
+}
+
+function iki() {
+    const crew1 = document.getElementById("crew1");
+    const crew2 = document.getElementById("crew2");
+    const crew3 = document.getElementById("crew3");
+    const btn2 = document.getElementById("btn2")
+
+    crew2.style.width = "100%";
+    crew2.style.display = "flex";
+
+    crew1.style.display = "none";
+    crew3.style.display = "none";
+}
+
+
+function uc() {
+    const crew1 = document.getElementById("crew1");
+    const crew2 = document.getElementById("crew2");
+    const crew3 = document.getElementById("crew3");
+    const btn3 = document.getElementById("btn3")
+
+    crew3.style.width = "100%";
+    crew3.style.display = "flex";
+
+    crew1.style.display = "none";
+    crew2.style.display = "none";
+}
+
+
+
+//###########################################   DESTİNATİON
+
+document.addEventListener("DOMContentLoaded", function () {
+    const moon1 = document.getElementById("moon1");
+    const mars1 = document.getElementById("mars1");
+    const europa1 = document.getElementById("europa1");
+    const titan1 = document.getElementById("titan1");
+
+    moon1.style.width = "100%";
+    moon1.style.display = "flex";
+
+
+
+    mars1.style.display = "none";
+    europa1.style.display = "none";
+    titan1.style.display = "none";
+});
+
+
+function moon2() {
+    const moon1 = document.getElementById("moon1");
+    const mars1 = document.getElementById("mars1");
+    const europa1 = document.getElementById("europa1");
+    const titan1 = document.getElementById("titan1");
+
+    moon1.style.width = "100%";
+    moon1.style.display = "flex";
+
+    mars1.style.display = "none";
+    europa1.style.display = "none";
+    titan1.style.display = "none";
+}
+
+function mars2() {
+    const moon1 = document.getElementById("moon1");
+    const mars1 = document.getElementById("mars1");
+    const europa1 = document.getElementById("europa1");
+    const titan1 = document.getElementById("titan1");
+
+    mars1.style.width = "100%";
+    mars1.style.display = "flex";
+
+    moon1.style.display = "none";
+    europa1.style.display = "none";
+    titan1.style.display = "none";
+}
+
+
+function europa2() {
+    const moon1 = document.getElementById("moon1");
+    const mars1 = document.getElementById("mars1");
+    const europa1 = document.getElementById("europa1");
+    const titan1 = document.getElementById("titan1");
+
+    europa1.style.width = "100%";
+    europa1.style.display = "flex";
+
+    moon1.style.display = "none";
+    mars1.style.display = "none";
+    titan1.style.display = "none";
+}
+
+
+
+function titan2() {
+    const moon1 = document.getElementById("moon1");
+    const mars1 = document.getElementById("mars1");
+    const europa1 = document.getElementById("europa1");
+    const titan1 = document.getElementById("titan1");
+
+    titan1.style.width = "100%";
+    titan1.style.display = "flex";
+
+    moon1.style.display = "none";
+    europa1.style.display = "none";
+    mars1.style.display = "none";
+}
+
+
+
+/* ##################################################### TECHNOLOGY */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const techno1 = document.getElementById("techno1");
+    const techno2 = document.getElementById("techno2");
+    const techno3 = document.getElementById("techno3");
+
+
+    techno1.style.width = "100%";
+    techno1.style.display = "flex";
+
+    techno2.style.display = "none";
+    techno3.style.display = "none";
+});
+
+
+function tikla1() {
+    const techno1 = document.getElementById("techno1");
+    const techno2 = document.getElementById("techno2");
+    const techno3 = document.getElementById("techno3");
+
+
+    techno1.style.width = "100%";
+    techno1.style.display = "flex";
+
+    techno2.style.display = "none";
+    techno3.style.display = "none";
+
+}
+
+
+
+function tikla2() {
+    const techno1 = document.getElementById("techno1");
+    const techno2 = document.getElementById("techno2");
+    const techno3 = document.getElementById("techno3");
+
+
+    techno2.style.width = "100%";
+    techno2.style.display = "flex";
+
+    techno1.style.display = "none";
+    techno3.style.display = "none";
+
+}
+
+
+
+
+
+function tikla3() {
+    const techno1 = document.getElementById("techno1");
+    const techno2 = document.getElementById("techno2");
+    const techno3 = document.getElementById("techno3");
+
+
+    techno3.style.width = "100%";
+    techno3.style.display = "flex";
+
+    techno2.style.display = "none";
+    techno1.style.display = "none";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
